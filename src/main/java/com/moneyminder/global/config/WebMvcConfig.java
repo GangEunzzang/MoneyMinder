ec590b4a09ff45e339b5eff220afaaff6df80e6c
이강은 <rkddms123456@gmail.com>
2024-07-26 17:39:07 +0900

    feat: WebMvcConfig 추가
    * argumentResolver 등록

---
+++ src/main/java/com/moneyminder/global/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.moneyminder.global.config;
+
+import com.moneyminder.domain.auth.jwt.JwtProvider;
+import com.moneyminder.global.resolver.CurrentUserEmailResolver;
+import com.moneyminder.global.resolver.CurrentUserResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig  implements WebMvcConfigurer {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new CurrentUserEmailResolver(jwtProvider));
+        resolvers.add(new CurrentUserResolver(jwtProvider));
+    }
+
+
+}

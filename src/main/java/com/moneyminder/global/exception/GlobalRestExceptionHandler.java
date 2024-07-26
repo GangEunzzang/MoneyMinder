03b7cae3efd50dab449e3649b5e1fc3faee4f800
이강은 <rkddms123456@gmail.com>
2024-07-26 17:40:46 +0900

    feat: ExceptionHandler 수정
    * Exception 발생시 error 로깅

--- src/main/java/com/moneyminder/global/exception/GlobalRestExceptionHandler.java
+++ src/main/java/com/moneyminder/global/exception/GlobalRestExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.moneyminder.global.exception;
 
 import com.moneyminder.global.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalRestExceptionHandler {
 
@@ -18,9 +20,10 @@ public class GlobalRestExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("Unexpected error occurred: ", e);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ErrorResponse.of(ResultCode.INTERNAL_ERROR));
+                .body(ErrorResponse.of(ResultCode.INTERNAL_ERROR, e.getMessage()));
     }
 
 }

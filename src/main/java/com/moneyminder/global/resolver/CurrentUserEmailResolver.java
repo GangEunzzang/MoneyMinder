dc3937f1392461930b14572351b8b26aee985217
이강은 <rkddms123456@gmail.com>
2024-07-26 17:40:06 +0900

    feat: UserEmailResolver 수정
    * jwt를 파싱 -> securityContextHolder에서 정보 추출

--- src/main/java/com/moneyminder/global/resolver/CurrentUserEmailResolver.java
+++ src/main/java/com/moneyminder/global/resolver/CurrentUserEmailResolver.java
@@ -1,11 +1,13 @@
 package com.moneyminder.global.resolver;
 
-import com.moneyminder.global.annotaion.CurrentUser;
 import com.moneyminder.domain.auth.jwt.JwtProvider;
+import com.moneyminder.global.annotaion.CurrentUser;
+import com.moneyminder.global.annotaion.CurrentUserEmail;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -18,12 +20,12 @@ public class CurrentUserEmailResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.getParameterAnnotation(CurrentUser.class) != null &&
+        return parameter.getParameterAnnotation(CurrentUserEmail.class) != null &&
                 String.class.isAssignableFrom(parameter.getParameterType());
     }
 
     @Override
-    public Object resolveArgument(@NotNull MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        return jwtProvider.getEmailByRequest((HttpServletRequest) webRequest.getNativeRequest());
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 }

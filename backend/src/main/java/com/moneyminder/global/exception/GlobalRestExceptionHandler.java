39fcfe0fd85c594d0465cc6eabf58b9a36332ed8
이강은 <rkddms123456@gmail.com>
2024-08-24 19:02:03 +0900

    [BE] feat: 글로벌 예외 추가
    * 유효성검증 예외 핸들링 추

--- backend/src/main/java/com/moneyminder/global/exception/GlobalRestExceptionHandler.java
+++ backend/src/main/java/com/moneyminder/global/exception/GlobalRestExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.moneyminder.global.exception;
 
 import com.moneyminder.global.response.ErrorResponse;
+import io.micrometer.core.instrument.config.validate.ValidationException;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,11 +22,31 @@ public class GlobalRestExceptionHandler {
                 .body(ErrorResponse.of(e.getResultCode(), e.getMessage()));
     }
 
+    @ExceptionHandler(ValidationException.class)
+    protected ResponseEntity<ErrorResponse> handleValidationException(ValidationException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(ErrorResponse.of(ResultCode.INVALID_INPUT_VALUE, e.getMessage()));
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ErrorResponse> bindException(BindException e) {
+        List<ObjectError> allErrors = e.getAllErrors();
+        String sb = allErrors.stream()
+                .map(error -> error.getDefaultMessage() + "\n")
+                .collect(Collectors.joining());
+
+        return ResponseEntity
+                .badRequest()
+                .body(ErrorResponse.of(ResultCode.INVALID_INPUT_VALUE, sb));
+    }
+
+
+
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ErrorResponse> handleException(Exception e) {
-        log.error("Unexpected error occurred: ", e);
         return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .internalServerError()
                 .body(ErrorResponse.of(ResultCode.INTERNAL_ERROR, e.getMessage()));
     }
 

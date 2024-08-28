d58805ebb163cdb2f216ab8085f48daee017f22e
이강은 <rkddms123456@gmail.com>
2024-08-28 23:02:20 +0900

    [INFRA] fix: 배포 스크립트 수정
    * 백엔드 환경변수 설정

--- backend/src/main/java/com/moneyminder/domain/auth/infrastructure/oauth2/handler/OAuth2FailureHandler.java
+++ backend/src/main/java/com/moneyminder/domain/auth/infrastructure/oauth2/handler/OAuth2FailureHandler.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 public class OAuth2FailureHandler extends SimpleUrlAuthenticationFailureHandler {
@@ -15,12 +14,12 @@ public class OAuth2FailureHandler extends SimpleUrlAuthenticationFailureHandler 
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException exception) throws IOException {
 
-        String frontRedirectUrl = request.getParameter("frontend_redirect_uri");
-
-        frontRedirectUrl = UriComponentsBuilder.fromUriString(frontRedirectUrl)
-                .queryParam("error", exception.getLocalizedMessage())
-                .build().toUriString();
-
-        getRedirectStrategy().sendRedirect(request, response, frontRedirectUrl);
+//        String frontRedirectUrl = request.getParameter("frontend_redirect_uri");
+//
+//        frontRedirectUrl = UriComponentsBuilder.fromUriString(frontRedirectUrl)
+//                .queryParam("error", exception.getLocalizedMessage())
+//                .build().toUriString();
+//
+//        getRedirectStrategy().sendRedirect(request, response, frontRedirectUrl);
     }
 }

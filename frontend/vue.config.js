3bf2b708fef20ae6c76431b2dc7e2faf63e8696d
이강은 <rkddms123456@gmail.com>
2024-09-01 21:01:34 +0900

    [FE] update: 백엔드 프록시주소 변경

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,11 +1,12 @@
 const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 3001,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080/api',
+        target: process.env.VUE_APP_API_BASE_URL || 'http://localhost:8080/api', // 환경 변수에서 API URL 가져오기
         changeOrigin: true,
         pathRewrite: { '^/api': '' },
       }

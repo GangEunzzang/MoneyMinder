f63dac70769cd4935c92837c8640a3cf8fc8e29d
이강은 <rkddms123456@gmail.com>
2025-03-07 17:34:33 +0900

    feat: api gateway 추가

---
+++ backend/api-gateway/src/main/java/com/moneyminder/filter/GlobalAuthFilter.java
@@ -0,0 +1,10 @@
+package com.moneyminder.filter;
+
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GlobalAuthFilter extends AbstractGatewayFilterFactory<GlobalAuthFilte> {
+
+
+}
